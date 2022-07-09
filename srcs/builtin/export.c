ea1f0605e40fc829f9dde6233e8d3d8e55899216
yubinquitous <yb9854@naver.com>
2022-07-09 16:34:38 +0900

    [FEAT] export - 리스트 복사 (#5)

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: yubin <yubchoi@student.42>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 17:01:53 by yubchoi           #+#    #+#             */
-/*   Updated: 2022/07/09 15:55:04 by yubin            ###   ########.fr       */
+/*   Updated: 2022/07/09 16:34:00 by yubin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,21 @@ t_env *init_env(char **env)
     t_env *lst;
     t_env *new;
     t_env *tmp;
+    int j;
 
     lst = (t_env *)malloc(sizeof(t_env));
+    lst->key = ft_strdup(env[0]);
     lst->value = ft_strdup(env[0]);
     lst->nxt = NULL;
     tmp = lst;
     while (env[i])
     {
+        j = 0;
+        while (env[i][j] != '=')
+            j++;
         new = (t_env *)malloc(sizeof(t_env));
-        new->value = ft_strdup(env[i]);
+        new->key = ft_substr(env[i], 0, j);
+        new->value = ft_substr(env[i], j + 1, ft_strlen(env[i]));
         new->nxt = NULL;
         tmp->nxt = new;
         tmp = tmp->nxt;
@@ -49,19 +55,19 @@ void check_export_syntax(int argc, char *str)
     }
 }
 
-t_env *sort_envp(t_env *envp)
-{
-    t_env *tmp;
-    t_env *left;
-    t_env *right;
-}
+// t_env *sort_envp(t_env *envp)
+// {
+//     t_env *tmp;
+//     t_env *left;
+//     t_env *right;
+// }
 
-t_env *dup_env(t_env *envp)
+t_env *dup_env(t_env *env)
 {
     t_env *dup_env;
 
     dup_env = (t_env *)ft_malloc(sizeof(t_env));
-    if (!envp)
+    if (!env)
     {
         dup_env->key = NULL;
         dup_env->value = NULL;
@@ -69,8 +75,8 @@ t_env *dup_env(t_env *envp)
     }
     else
     {
-        dup_env->key = ft_strdup(envp->key);
-        dup_env->value = ft_strdup(envp->value);
+        dup_env->key = ft_strdup(env->key);
+        dup_env->value = ft_strdup(env->value);
         dup_env->nxt = NULL;
     }
     return (dup_env);
@@ -79,8 +85,24 @@ t_env *dup_env(t_env *envp)
 t_env *dup_envp(t_env *envp)
 {
     t_env *dup_envp;
-    t_env new;
-    t_env tmp;
+    t_env *tmp;
+
+    dup_envp = (t_env *)ft_malloc(sizeof(t_env));
+    dup_envp = dup_env(envp);
+    if (dup_envp->key)
+    {
+        tmp = dup_envp;
+        while (envp->nxt)
+        {
+            envp = envp->nxt;
+            tmp->nxt = (t_env *)ft_malloc(sizeof(t_env));
+            tmp->nxt->key = ft_strdup(envp->key);
+            tmp->nxt->value = ft_strdup(envp->value);
+            tmp->nxt->nxt = NULL;
+            tmp = tmp->nxt;
+        }
+    }
+    return (dup_envp);
 }
 
 void print_all_envp(t_env *envp)
@@ -89,14 +111,19 @@ void print_all_envp(t_env *envp)
     t_env *tmp;
 
     tmp = dup_envp(envp);
-    sorted_envp = sort_envp(envp);
+    // while (tmp)
+    // {
+    //     printf("%s=%s\n", tmp->key, tmp->value);
+    //     tmp = tmp->nxt;
+    // }
+    // sorted_envp = sort_envp(envp);
 }
 
 void do_export(int argc, char **argv, t_env *envp)
 {
     if (argc == 1)
         print_all_envp(envp);
-    check_export_syntax(argc, argv);
+    // check_export_syntax(argc, argv);
 }
 
 int main(int argc, char **argv, char **env)
