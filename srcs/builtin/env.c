989da55cfc5039d52239e83b7d767d9414325cb5
yubinquitous <yb9854@naver.com>
2022-07-09 13:25:07 +0900

    [FEAT] env 함수 구현 (#12)

--- srcs/builtin/env.c
+++ srcs/builtin/env.c
@@ -6,12 +6,57 @@
 /*   By: yubin <yubchoi@student.42>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 12:57:34 by yubin             #+#    #+#             */
-/*   Updated: 2022/07/09 12:57:58 by yubin            ###   ########.fr       */
+/*   Updated: 2022/07/09 13:24:28 by yubin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int main(void)
+t_env *init_env(char **env)
 {
+    int i = 1;
+    t_env *lst;
+    t_env *new;
+    t_env *tmp;
+
+    lst = (t_env *)malloc(sizeof(t_env));
+    lst->value = ft_strdup(env[0]);
+    lst->nxt = NULL;
+    tmp = lst;
+    while (env[i])
+    {
+        new = (t_env *)malloc(sizeof(t_env));
+        new->value = ft_strdup(env[i]);
+        new->nxt = NULL;
+        tmp->nxt = new;
+        tmp = tmp->nxt;
+        i++;
+    }
+    return (lst);
+}
+
+void do_env(t_env *envp)
+{
+    t_env *tmp;
+
+    tmp = envp;
+    while (tmp)
+    {
+        printf("%s\n", tmp->value);
+        tmp = tmp->nxt;
+    }
+}
+
+int main(int ac, char **av, char **env)
+{
+    t_env *envp;
+
+    envp = init_env(env);
+    if (ac > 1)
+    {
+        printf("env: %s: No such file or directory\n", av[1]);
+        exit(1);
+    }
+    do_env(envp);
+    exit(0);
 }
