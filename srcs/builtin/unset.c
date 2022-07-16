9593a0fb2475d857abc488a22aaa6dd8bfe294fa
yubinquitous <yb9854@naver.com>
2022-07-16 15:29:19 +0900

    [CHORE] test용 출력문 삭제 (#39)

--- srcs/builtin/unset.c
+++ srcs/builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: yubchoi <yubchoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 13:15:38 by yubin             #+#    #+#             */
-/*   Updated: 2022/07/15 13:50:57 by yubchoi          ###   ########.fr       */
+/*   Updated: 2022/07/16 15:29:04 by yubchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,6 @@ void ft_unset(t_node *node)
     {
         if (is_invalid_key(ft_strdup(node->cmd[i])))
             continue;
-        printf("%s\n", node->cmd[i]);
         node->envp = ft_unset_env(node->envp, node->cmd[i]);
     }
 }
