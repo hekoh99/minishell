c1f9c669ace0ab168227c75436edcf2da1f409d3
yubinquitous <yb9854@naver.com>
2022-07-16 14:28:22 +0900

    [FIX] 잘못된 명령어 에러 메시지 수정 (#39)

--- srcs/utils/ft_system_call2.c
+++ srcs/utils/ft_system_call2.c
@@ -6,16 +6,24 @@
 /*   By: yubchoi <yubchoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 10:45:29 by yubchoi           #+#    #+#             */
-/*   Updated: 2022/07/16 10:47:43 by yubchoi          ###   ########.fr       */
+/*   Updated: 2022/07/16 14:26:50 by yubchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/utils.h"
 
+extern int g_stat;
+
 void	ft_execve(char *file, char **argv, char **envp)
 {
+	char *msg;
+
 	if (!file)
-		error_exit("Command not found", 127);
-	if (execve(file, argv, envp) == -1)
-		error_exit("execve failed", 1);
+	{
+		msg = ft_strjoin(argv[0], ": command not found\n");
+		write(2, msg, ft_strlen(msg));
+		g_stat = 127;
+	}
+	else if (execve(file, argv, envp) == -1)
+		error_exit(argv[0], 1);
 }
