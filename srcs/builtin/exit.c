856e7347759c0ad3ce8a7c149d941526010e9671
yubinquitous <yb9854@naver.com>
2022-07-18 14:54:29 +0900

    [CHORE] 테스트 코드 삭제 (#39)

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: yubin <yubchoi@student.42>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 13:00:56 by yubin             #+#    #+#             */
-/*   Updated: 2022/07/18 14:08:55 by yubin            ###   ########.fr       */
+/*   Updated: 2022/07/18 14:53:01 by yubin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,10 +47,7 @@ void ft_exit(int single_cmd, t_node *node)
     if (single_cmd)
         printf("exit\n");
     if (node->cmd[1] == NULL)
-    {
-        printf("g_stat: %d\n", g_stat); // test
         exit(g_stat);
-    }
     validate_exit_code(node->cmd[1]);
     status_ll = ft_atoll(node->cmd[1]);
     g_stat = status_ll % 256 + 256 * (status_ll < 0);
