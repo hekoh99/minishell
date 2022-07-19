bbfe976c86d4d16edc877e97c917ab4e77760257
yubinquitous <yb9854@naver.com>
2022-07-19 13:13:46 +0900

    [FIX] 에러 메시지 수정 (#39)

--- srcs/utils/ft_system_call2.c
+++ srcs/utils/ft_system_call2.c
@@ -6,7 +6,7 @@
 /*   By: yubin <yubchoi@student.42>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 10:45:29 by yubchoi           #+#    #+#             */
-/*   Updated: 2022/07/18 14:01:29 by yubin            ###   ########.fr       */
+/*   Updated: 2022/07/19 13:11:55 by yubin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ extern int g_stat;
 
 void ft_execve(char *file, char **argv, char **envp)
 {
-	char *msg;
-
 	if (!file)
 		print_error2(ft_strdup(argv[0]), ft_strdup(": No such file or directory\n"), 127);
+	else if (!argv || !*argv || !*argv[0])
+		print_error(ft_strjoin(argv[0], ": command not found\n"), 127);
 	else if (execve(file, argv, envp) == -1)
 		print_error(ft_strjoin(argv[0], ": is a directory\n"), 126);
 }
