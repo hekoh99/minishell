3ee141267fbc1c83836022817b3141a08df406bd
haeunko <hekoh99@naver.com>
2022-07-22 20:01:25 +0900

    expand 25

--- srcs/expand.c
+++ srcs/expand.c
@@ -52,14 +52,38 @@ static void set_expanded_value(t_token *token, char *replaced, int start, int *i
     (*index) = (*index) - ((*index) - start + 1);
 }
 
+void expand_home_var(t_token *tmp, t_env *env, int *index)
+{
+    char *replaced;
+
+    replaced = search_env(env, "HOME");
+    if (!replaced) // HOME 없을 때
+        replaced = ft_strdup(getenv("HOME"));
+    (*index)++;
+    set_expanded_value(tmp, replaced, 1, index);
+}
+
+void expand_env_var(t_token *tmp, t_env *env, int *index)
+{
+    int start;
+    char *target;
+    char *replaced;
+
+    start = (*index) + 1;
+    (*index)++;
+    while ((ft_isalpha(tmp->value[*index]) == 1 || ft_isdigit(tmp->value[*index])) && tmp->value[*index] != '\0')
+        (*index)++;
+    target = ft_substr(tmp->value, start, (*index) - start);
+    replaced = search_env(env, target);
+    free(target);
+    set_expanded_value(tmp, replaced, start, index);
+}
+
 t_token *expand(t_token *token, t_env *env) // parse $ ~ 작은 따옴표 안은 무시
 {
     int i;
     int squote;
     int dquote;
-    int start;
-    char *target;
-    char *replaced;
     t_token *tmp;
 
     tmp = token;
@@ -72,24 +96,9 @@ t_token *expand(t_token *token, t_env *env) // parse $ ~ 작은 따옴표 안은
         {
             check_quote(tmp->value[i], &squote, &dquote);
             if (tmp->value[i] == '$' && tmp->value[i + 1] != '\0' && squote == 0)
-            {
-                start = i + 1;
-                i++;
-                while ((ft_isalpha(tmp->value[i]) == 1 || ft_isdigit(tmp->value[i])) && tmp->value[i] != '\0')
-                    i++;
-                target = ft_substr(tmp->value, start, i - start);
-                replaced = search_env(env, target);
-                free(target);
-                set_expanded_value(tmp, replaced, start, &i);
-            }
+                expand_env_var(tmp, env, &i);
             else if (i == 0 && tmp->value[i] == '~' && (ft_strlen(tmp->value) == 1 || tmp->value[i + 1] == '/') && squote == 0)
-            {
-                replaced = search_env(env, "HOME");
-                if (!replaced) // HOME 없을 때
-                    replaced = ft_strdup(getenv("HOME"));
-                i++;
-                set_expanded_value(tmp, replaced, 1, &i);
-            }
+                expand_home_var(tmp, env, &i);
             else
                 i++;
         }
