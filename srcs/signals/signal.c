615c7ec1dc842ec34917cc400bbb628d2c1f4d38
yubinquitous <yb9854@naver.com>
2022-07-23 13:29:34 +0900

    [FIX] git 충돌 해결 (#39)

--- srcs/signals/signal.c
+++ srcs/signals/signal.c
@@ -6,7 +6,7 @@
 /*   By: yubchoi <yubchoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 16:29:32 by hako              #+#    #+#             */
-/*   Updated: 2022/07/23 13:26:50 by yubchoi          ###   ########.fr       */
+/*   Updated: 2022/07/23 13:28:57 by yubchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,32 @@ extern int	g_stat;
 void	child_sig_int(int signal)
 {
     if (signal == SIGINT)
-        write(2, "^C\n", 3);
+	    write(2, "^C\n", 3);
+}
+
+void	sig_int(int signal)
+{
+	struct termios	attributes;
+	struct termios	saved;
+
+	tcgetattr(STDIN_FILENO, &saved);
+	tcgetattr(STDIN_FILENO, &attributes);
+	attributes.c_lflag &= (~ECHOCTL);
+	tcsetattr(STDIN_FILENO, TCSANOW, &attributes);
+	if (signal != SIGINT)
+		return;
+	g_stat = ETC;
+	ioctl(STDIN_FILENO, TIOCSTI, "\n");
+	rl_on_new_line();
+	rl_replace_line("", 1);
+}
+
+void heredoc_sig_int(int signal)
+{
+    if (signal != SIGINT)
+        return;
+    ioctl(STDIN_FILENO, TIOCSTI, "\n");
+    rl_on_new_line();
+    rl_replace_line("", 1);
+    g_stat = ETC;
 }
