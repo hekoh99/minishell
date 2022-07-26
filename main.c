44e65e757618c8e3d6ff8187fea3b3840d773428
haeunko <hekoh99@naver.com>
2022-07-26 21:38:54 +0900

    fixed ctrlD ctrlC

--- main.c
+++ main.c
@@ -46,6 +46,7 @@ void	minishell(t_env *envp)
 
 	token = NULL;
 	node = NULL;
+	write(1, "\033[u", 3);
 	str = readline("minishell$ ");
 	if (!str)
 		handle_eof_exit(envp);
@@ -71,6 +72,7 @@ int	main(int ac, char **av, char **env)
 
 	ac += 0;
 	av += 0;
+	write(1, "\033[s", 3);
 	envp = init_env(env);
 	g_stat = 0;
 	while (1)
