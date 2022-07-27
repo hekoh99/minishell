b470edd2569d42b320ef8c9547d65d4ce418e239
yubinquitous <yb9854@naver.com>
2022-07-27 14:57:45 +0900

    [FEAT] lemon 추가!

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hako <hako@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: yubin <yubchoi@student.42>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 11:31:30 by hako              #+#    #+#             */
-/*   Updated: 2022/07/27 11:30:15 by hako             ###   ########.fr       */
+/*   Updated: 2022/07/27 14:57:17 by yubin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,55 @@ void	minishell(t_env *envp)
 	}
 }
 
+void	write_with_color(int fd, char *str, int len)
+{
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		if (str[i] == '+')
+		{
+			write(1, "\033[31m빨간 휴지 줄까??? \033[34m파랑 휴지 줄까???\033[0m", ft_strlen("\033[31m빨간 휴지 줄까??? \033[34m파랑 휴지 줄까???\033[0m"));
+			i++;
+			write(fd, &str[i], 1);
+			i++;
+			write(fd, &str[i], 1);
+			i++;
+		}
+		else
+		{
+			write(fd, &str[i], 1);
+			i++;
+		}
+	}
+}
+
+void	print_with_color(int fd, char *str, int len)
+{
+	int	i;
+
+	i = -1;
+	while (++i < len)
+	{
+		if (str[i] == '+')
+			printf("\033[31m+\033[0m");
+		else if (str[i] == '*')
+			printf("%c", str[i]);
+	}
+}
+
+void	print_lemon(void)
+{
+	char	buf[500001];
+	int		length;
+	int		fd;
+	
+	fd = open("lemon", O_RDONLY);
+	length = read(fd, buf, 500000);
+	write(1, buf, length);
+}
+
 int	main(int ac, char **av, char **env)
 {
 	char	*str;
@@ -73,10 +122,9 @@ int	main(int ac, char **av, char **env)
 
 	ac += 0;
 	av += 0;
-	write(1, "\033[s", 3);
-	write(1, "\033[u", 3);
 	envp = init_env(env);
 	g_stat = 0;
+	print_lemon();
 	while (1)
 	{
 		signal(SIGINT, sig_int);
